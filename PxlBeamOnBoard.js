a981df2a6a106a1a63f58e0d2a77f00cb43d3652
Gayane Nerkararyan <imagin1@yandex.ru>
2020-01-15 17:55:10 +0400

    Add files via upload

---
+++ PxlBeamOnBoard.js
@@ -0,0 +1,10 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'PxlBeamOnBoard', 'coolMethod', [arg0]);
+};
+window.echo = function(str, callback) {
+    cordova.exec(callback, function(err) {
+        callback('Nothing to echo.');
+    }, "Echo", "echo", [str]);
+};
